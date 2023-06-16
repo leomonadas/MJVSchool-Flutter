b0215cdbc4cec8947fb4bc23ab805a5eeb5f17c3
Leonardo Alves <77860170+leomonadas@users.noreply.github.com>
2023-06-15 21:28:45 -0300

    Metade da aula 07/06

--- Projeto_02/school/lib/app_widget.dart
+++ Projeto_02/school/lib/app_widget.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
@@ -6,17 +5,35 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
-      theme: CupertinoThemeData(brightness: Brightness.light),
-      home: CupertinoPageScaffold(
-        child: Center(
-          child: CupertinoButton.filled(
-            child: Text('Botão'), 
-            onPressed: (){},
-          ),
+    return MaterialApp(
+      title: 'School',
+      home: Scaffold(
+        body: Container(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+            const CircleAvatar(child: Text('L'),),
+            const Text('Texto 01'),
+            const Text('Texto 0'),
+            const Text('Texto 03'),
+            const Divider(), //adiciona linha
+            const Row(
+              children: [
+                Icon(Icons.add, color: Colors.green,), //adiciona icon
+                Spacer(),             //adiciona espaço
+                Icon(Icons.access_alarms, color: Colors.yellow),
+              ],
+            ),
+            Switch(value: true, onChanged: (val){}), //Esse 'onChanged: (){}' é uma função anonima! É o botão de switch
+            const Card(
+              child: Padding(padding: EdgeInsets.all(16), child: Text('Textinho do card!'))
+            ),
+          ],
         ),
       ),
-    );
+    ));
   }
 }
 
