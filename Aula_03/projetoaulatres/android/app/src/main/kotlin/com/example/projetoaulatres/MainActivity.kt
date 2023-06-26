639e0ff65774385618eba20ade1056f415668b20
Leonardo Alves <77860170+leomonadas@users.noreply.github.com>
2023-06-26 12:06:09 -0300

    Aula 03

---
+++ Aula_03/projetoaulatres/android/app/src/main/kotlin/com/example/projetoaulatres/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.projetoaulatres
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Aula_03/projetoaulatres/lib/app_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:projetoaulatres/pages/home/home_page.dart';
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'School',
+      home: HomePage(),
+    );
+  }
+}
+
